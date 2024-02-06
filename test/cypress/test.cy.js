7f97a2d11b0e954fcacc1f9558a940164822f14f
Natsuki Fujii <natsuki.fujii@rakus-partners.co.jp>
2024-02-06 15:04:43 +0900

    一覧画面のJavaScript修正

--- test/cypress/test.cy.js
+++ test/cypress/test.cy.js
@@ -25,29 +25,33 @@ describe('顧客情報入力フォームのテスト', () => {
 
 describe('顧客一覧画面のテスト', () => {
   //テストデータ
-  const testData = { 
-    customer_id : 1, 
-    company_name : 'テスト株式会社', 
-    industry : 'IT', 
-    contact : '03-1111-1111', 
-    location : '東京都新宿区', 
-    created_date : Date.now(), 
-    updated_date : Date.now() 
-  };
+  const testData = [
+    {
+    customer_id : 1,
+    company_name : 'テスト株式会社',
+    industry : 'IT',
+    contact : '03-1111-1111',
+    location : '東京都新宿区',
+    created_date : Date.now(),
+    updated_date : Date.now()
+    }
+  ];
 
-  it('データを取得し、顧客一覧ページを表示する', () => {
-    cy.intercept('GET', '/api_natsuki_fujii/customers', testData);
-    cy.visit('/natsuki_fujii/customer/list.html'); //テスト対象のページにアクセス
-
-    cy.window().then((win) => {
-      createTable(testData);
-    });
+  it('顧客一覧ページを表示し、会社名をクリックすると詳細ページに遷移する', () => {
+    cy.intercept('GET', '/api_natsuki_fujii/customers', testData).as('customerData');
+    cy.visit('/natsuki_fujii/customer/list.html'); // テスト対象のページにアクセス
+    cy.wait('@customerData');
 
     cy.get('#customer-list tr').each(($row, index) => {
       //各セルにテストデータが入っているか確認
         cy.wrap($row).find('td').eq(0).should('contain', index + 1);
-        cy.wrap($row).find('td').eq(1).should('contain', testData.companyName);
-        cy.wrap($row).find('td').eq(2).should('contain', testData.contact);
+        cy.wrap($row).find('td').eq(1).should('contain', testData[0].company_name);
+        cy.wrap($row).find('td').eq(2).should('contain', testData[0].contact);
     });
+
+    cy.intercept('POST', '/api_natsuki_fujii/company', testData[0].company_name).as('customerName');
+    cy.get('a').click();
+    cy.wait('@customerName');
+    cy.url().should('include', '/natsuki_fujii/customer/detail.html');
   });
 });
