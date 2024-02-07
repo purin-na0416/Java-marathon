b3792047f03174469ab78bdebdd530e8c65278d0
Natsuki Fujii <natsuki.fujii@rakus-partners.co.jp>
2024-02-07 09:42:19 +0900

    詳細画面のJavaScript修正

--- test/cypress/test.cy.js
+++ test/cypress/test.cy.js
@@ -1,3 +1,19 @@
+const { describe } = require("node:test");
+
+//テストデータ
+const testData = [
+  {
+  customer_id : 1,
+  company_name : 'テスト株式会社',
+  industry : 'IT',
+  contact : '03-1111-1111',
+  location : '東京都新宿区',
+  created_date : Date.now(),
+  updated_date : Date.now()
+  }
+];
+
+
 describe('顧客情報入力フォームのテスト', () => {
   it('顧客情報を入力して送信し、成功メッセージを確認する', () => {
     cy.visit('/nishi/customer/add.html'); // テスト対象のページにアクセス
@@ -5,7 +21,6 @@ describe('顧客情報入力フォームのテスト', () => {
       // windowのalertをスタブ化し、エイリアスを設定
       cy.stub(win, 'alert').as('alertStub');
     });
-
 
     // テストデータの読み込み
     cy.fixture('customerData').then((data) => {
@@ -23,35 +38,35 @@ describe('顧客情報入力フォームのテスト', () => {
 });
 
 
-describe('顧客一覧画面のテスト', () => {
-  //テストデータ
-  const testData = [
-    {
-    customer_id : 1,
-    company_name : 'テスト株式会社',
-    industry : 'IT',
-    contact : '03-1111-1111',
-    location : '東京都新宿区',
-    created_date : Date.now(),
-    updated_date : Date.now()
-    }
-  ];
-
-  it('顧客一覧ページを表示し、会社名をクリックすると詳細ページに遷移する', () => {
+describe('顧客一覧画面と顧客詳細画面のテスト', () => {
+  it('顧客一覧ページを表示し、会社名をクリックすると詳細画面に遷移する', () => {
     cy.intercept('GET', '/api_natsuki_fujii/customers', testData).as('customerData');
     cy.visit('/natsuki_fujii/customer/list.html'); // テスト対象のページにアクセス
     cy.wait('@customerData');
 
     cy.get('#customer-list tr').each(($row, index) => {
       //各セルにテストデータが入っているか確認
-        cy.wrap($row).find('td').eq(0).should('contain', index + 1);
-        cy.wrap($row).find('td').eq(1).should('contain', testData[0].company_name);
-        cy.wrap($row).find('td').eq(2).should('contain', testData[0].contact);
+      cy.wrap($row).find('td').eq(0).should('contain', index + 1);
+      cy.wrap($row).find('td').eq(1).should('contain', testData[0].company_name);
+      cy.wrap($row).find('td').eq(2).should('contain', testData[0].contact);
     });
 
-    cy.intercept('POST', '/api_natsuki_fujii/company', testData[0].company_name).as('customerName');
+    cy.intercept('POST', '/api_natsuki_fujii/company', { success : true }).as('customerName');
     cy.get('a').click();
     cy.wait('@customerName');
+
+    //詳細画面に遷移できているか確認
+    cy.intercept('POST', '/api_natsuki_fujii/detail', { success : true, companyData : testData[0] }).as('customerDetail');
     cy.url().should('include', '/natsuki_fujii/customer/detail.html');
+    cy.wait('@customerDetail');
+
+    //各項目にテストデータが入っているか確認
+    cy.get('#customer_id').should('contain', testData[0].customer_id);
+    cy.get('#company_name').should('contain', testData[0].company_name);
+    cy.get('#industry').should('contain', testData[0].industry);
+    cy.get('#contact').should('contain', testData[0].contact);
+    cy.get('#location').should('contain', testData[0].location);
+    cy.get('#created_date').should('contain', testData[0].created_date);
+    cy.get('#updated_date').should('contain', testData[0].updated_date);
   });
 });
